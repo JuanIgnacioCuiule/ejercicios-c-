1ea73b74bc58de7b6b9e5f006fbb645360f71f27
Juan Ignacio Cuiule <juanignaciocuiule@gmail.com>
2016-10-19 23:08:43 -0300

    armado del arbol

--- arbolRegistros/main.cpp
+++ arbolRegistros/main.cpp
@@ -1,17 +1,50 @@
 #include <iostream>
-
 using namespace std;
 
 struct Nodo {
-	int valor;
-	Nodo* Hizq;
-	Nodo* Hder;
+	int dato;
+	Nodo* izq;
+	Nodo* der;
 };
 
-int main(){
-	Nodo* I;
+void insertar(Nodo* &raiz, int dato) {
+	if(raiz == NULL) {
+		Nodo* aux = new Nodo;
+		aux->dato = dato;
+		aux->izq = NULL;
+		aux->der = NULL;
+		raiz = aux;
+	} else {
+		if(dato < raiz->dato) {
+			insertar(raiz->izq, dato);
+		} else if(dato > raiz->dato) {
+			insertar(raiz->der, dato);
+		} else {
+			cout << dato << " no entro por estar repetido" << endl;
+		}
+	}
+	return;
+}
 
-	cout << "Ingrese el valor del primer nodo: ";
-	cin >> I->valor;
+void inOrden(Nodo* arbol){
+	if (arbol != NULL) { 
+		inOrden(arbol->izq);
+		cout << arbol->dato;
+		inOrden(arbol->der);
+	}
+	return; 
+}
 
+int main() {
+	
+	Nodo* raiz = NULL;
+	
+	insertar(raiz, 10);
+	insertar(raiz, 5);
+	insertar(raiz, 20);
+	insertar(raiz, 3);
+	insertar(raiz, 4);
+	insertar(raiz, 1);
+	insertar(raiz, 6);
+	
 }
