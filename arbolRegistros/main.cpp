ab435f79647318e6ca255be63af95c7926c93816
Juan Ignacio Cuiule <juanignaciocuiule@gmail.com>
2016-10-20 03:37:26 -0300

    ABB - listo?

--- arbolRegistros/main.cpp
+++ arbolRegistros/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <math.h>
+#include <time.h>
 using namespace std;
 
 struct Nodo {
@@ -7,44 +9,169 @@ struct Nodo {
 	Nodo* der;
 };
 
-void insertar(Nodo* &raiz, int dato) {
-	if(raiz == NULL) {
+void insertar(Nodo* &arbol, int elem) {
+	if (arbol == NULL) {
 		Nodo* aux = new Nodo;
-		aux->dato = dato;
+		aux->dato = elem;
 		aux->izq = NULL;
 		aux->der = NULL;
-		raiz = aux;
+		arbol = aux;
 	} else {
-		if(dato < raiz->dato) {
-			insertar(raiz->izq, dato);
-		} else if(dato > raiz->dato) {
-			insertar(raiz->der, dato);
+		if (elem < arbol->dato) {
+			insertar(arbol->izq, elem);
+		} else if (elem > arbol->dato) {
+			insertar(arbol->der, elem);
 		} else {
-			cout << dato << " no entro por estar repetido" << endl;
+			cout << elem << " no entro por estar repetido" << endl;
 		}
 	}
 	return;
 }
 
+void reemplazar(Nodo* &arbol, Nodo* &aux) {
+	if (arbol->der == NULL) {
+		aux->dato = arbol->dato;
+		aux = arbol;
+		arbol = arbol->izq;
+	} else {
+		reemplazar(arbol->der, aux);
+	}
+}
+
+void borrar(Nodo* &arbol, int elem) {
+		Nodo* aux = new Nodo;
+		if (arbol == NULL)
+			return;
+			
+		if (elem < arbol->dato)
+			borrar(arbol->izq, elem);
+			
+		else if (elem > arbol->dato)
+			borrar(arbol->der, elem);
+			
+		else if (elem == arbol->dato) {
+			
+			aux = arbol;
+			if (arbol->izq == NULL)
+				arbol = arbol->der;
+				
+			else if (arbol->der == NULL)
+				arbol = arbol->izq;
+				
+			else
+				reemplazar(arbol->izq, aux);
+			
+			
+		}
+}
+
 void inOrden(Nodo* arbol){
 	if (arbol != NULL) { 
 		inOrden(arbol->izq);
-		cout << arbol->dato;
+		cout << " - " << arbol->dato;
 		inOrden(arbol->der);
 	}
 	return; 
 }
 
+void preOrden(Nodo* arbol){ 
+	if (arbol != NULL) { 
+		cout << " - " << arbol->dato;
+		preOrden(arbol->izq);
+		preOrden(arbol->der);
+	}
+	return; 
+}
+
+void posOrden(Nodo* arbol){ 
+	if (arbol != NULL) { 
+		posOrden(arbol->izq);
+		posOrden(arbol->der);
+		cout << " - " << arbol->dato;
+	}
+	return; 
+}
+
+bool buscar(Nodo* arbol, int elem) {
+	if (arbol == NULL) {
+		return 0;
+	} else if (elem < arbol->dato) {
+		buscar(arbol->izq, elem);
+	} else if (elem > arbol->dato) {
+		buscar(arbol->der, elem);
+	} else {
+		return 1;
+	}
+}
+
+void bubble(int vec_des[])  {
+	int i, j;
+	int aux;
+	int ordenado = 0;
+	for( i = 0; i < 10 - 1 && ordenado == 0; i++){
+		ordenado = 1;
+		for( j = 0; j < 10 - 1 - i; j++){
+			if( vec_des[j] > vec_des[j+1]){
+				ordenado = 0;
+				aux = vec_des[j];
+				vec_des[j] = vec_des[j+1];
+				vec_des[j+1] = aux;
+			}
+		}
+	}
+}
+
+
+
 int main() {
 	
-	Nodo* raiz = NULL;
+	srand(time(NULL));
 	
-	insertar(raiz, 10);
-	insertar(raiz, 5);
-	insertar(raiz, 20);
-	insertar(raiz, 3);
-	insertar(raiz, 4);
-	insertar(raiz, 1);
-	insertar(raiz, 6);
+	Nodo* raiz = NULL;
+	int busq = 0;
+	int e_borrar = 0;
+	
+	int v[10];
+	
+	for(int a = 0; a < 10; a++) {
+		v[a] = rand()%100;
+	}
+	
+	bubble(v);
+	
+	insertar(raiz, v[4]);
+	for(int a = 0; a < 10; a++) {
+		insertar(raiz, v[a]);
+	}
+	
+	cout << "Ingrese el numero que quiere buscar: ";
+	cin >> busq;
+	
+	if (buscar(raiz, busq)) {
+		cout << "El elemento " << busq << " se encuentra en el arbol" << endl;
+	} else {
+		cout << "El elemento " << busq << " NO se encuentra en el arbol" << endl;
+	}
+	
+	cout << "inOrden: ";
+	inOrden(raiz);
+	cout << "\n posOrden: ";
+	posOrden(raiz);
+	cout << "\n preOrden: ";
+	preOrden(raiz);
+	cout << "\n";
+	
+	cout << "Ingrese el numero que quiere borrar: ";
+	cin >> e_borrar;
+	borrar(raiz, e_borrar);
+	
+	cout << "inOrden: ";
+	inOrden(raiz);
+	cout << "\n posOrden: ";
+	posOrden(raiz);
+	cout << "\n preOrden: ";
+	preOrden(raiz);
+	cout << "\n";
+	
 	
 }
