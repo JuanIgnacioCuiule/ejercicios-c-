c10cc7ed1f9f821324eba14b4bfbef1e4a7f1204
Juan Ignacio Cuiule <juanignaciocuiule@gmail.com>
2017-09-20 13:42:15 -0300

    agrego strcpy para tener cadenas en los nodos de la lista

--- Tp-Sintaxis/scanner-lista/scanner.c
+++ Tp-Sintaxis/scanner-lista/scanner.c
@@ -11,15 +11,26 @@ typedef enum {
 
 typedef struct Nodo {
   TOKEN token;
-  //char cadena[33];
-  char cadena;
+  char cadena[33];
+  //char cadena;
   struct Nodo* sig; 
 } Nodo;
 
-void agregar(Nodo** cola, TOKEN token, char cadena) {
+void strcpy(char destino[], char origen[]) {
+  int largoDestino = (sizeof(destino)/sizeof(char));
+  int largoOrigen = (sizeof(destino)/sizeof(char));
+  for (int i = 0; i < largoOrigen && i < largoDestino; ++i) {
+    destino[i] = origen[i];
+  }
+}
+
+void agregar(Nodo** cola, TOKEN token, char cadena[]) {
   Nodo* nuevo = (Nodo*)malloc(sizeof(Nodo));
   nuevo->token = token;
-  nuevo->cadena = cadena;
+
+  nuevo->cadena[0] = cadena[0];
+  strcpy(nuevo->cadena, cadena);
+  //nuevo->cadena = cadena;
   if (*cola == NULL) {
     *cola = nuevo;
   } else {
@@ -33,7 +44,7 @@ void agregar(Nodo** cola, TOKEN token, char cadena) {
 
 void mostrar(Nodo* lista) {
   while (lista != NULL) {
-    printf("%c\n", lista->cadena);
+    printf("%s\n", lista->cadena);
     printf("%d\n", lista->token);
     lista = lista->sig;
   }
@@ -41,10 +52,10 @@ void mostrar(Nodo* lista) {
 
 int main () {
   Nodo* cola1 = NULL;
-  agregar(&cola1, INICIO, 'a');
-  agregar(&cola1, LEER, 'b');
-  agregar(&cola1, PUNTOYCOMA, 'c');
-  agregar(&cola1, FDT, 'd');
+  agregar(&cola1, INICIO, "inicio");
+  agregar(&cola1, LEER, "leer");
+  agregar(&cola1, PUNTOYCOMA, ";");
+  agregar(&cola1, FDT, "z");
   mostrar(cola1);
 
   return 0; 
