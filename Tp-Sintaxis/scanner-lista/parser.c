03d414144fe236e094edd4da4311a2db9771a6f2
Juan Ignacio Cuiule <juanignaciocuiule@gmail.com>
2017-10-10 18:13:15 -0300

    lista sentencias, sentencia, lista identificadores

--- Tp-Sintaxis/scanner-lista/parser.c
+++ Tp-Sintaxis/scanner-lista/parser.c
@@ -29,12 +29,58 @@ void objetivo() {
 
 void programa() {
   // <programa> -> INICIO <listaSentencias> FIN
-  TOKEN t;
   match(INICIO);
-  printf("%s %s\n", tokens[nodoActual->token], nodoActual->cadena);
-  while((t = proximoToken()) != FIN) {}
-  printf("%s %s\n", tokens[nodoActual->token], nodoActual->cadena);
-  //match(FIN);
+  listaSentencias();
+  match(FIN);
+}
+
+void listaSentencias() {
+  TOKEN t = proximoToken();
+  while(t == ID || t == LEER || t == ESCRIBIR) {
+    sentencia(t);
+    printf("%s %s\n", tokens[nodoActual->token], nodoActual->cadena);
+    t = proximoToken();
+  }
+  return;
+}
+// INICIO, FIN, LEER, ESCRIBIR, ID, CONSTANTE, PARENIZQUIERDO, PARENDERECHO,
+// PUNTOYCOMA, COMA, ASIGNACION, SUMA, RESTA, FDT, ERRORLEXICO
+void sentencia(TOKEN t) {
+  switch(t) {
+    case ID:
+      printf("sentencia por id\n");
+      match(ASIGNACION);
+      while(proximoToken() != PUNTOYCOMA){};
+      break;
+      // expresion();
+      // match(PUNTOYCOMA);
+    case LEER:
+      printf("sentencia por leer\n");
+      match(PARENIZQUIERDO);
+      listaIdentificadores();
+      //match(PARENDERECHO);
+      match(PUNTOYCOMA);
+      break;
+    case ESCRIBIR:
+      printf("sentencia por ecribir\n");
+      match(PARENIZQUIERDO);
+      while(proximoToken() != PARENDERECHO){};
+      // listaExpresiones();
+      // match(PARENDERECHO);
+      match(PUNTOYCOMA);
+      break;
+    default:
+      errorSintactico();
+  }
+  return;
+}
+
+void listaIdentificadores() {
+  match(ID);
+  for (TOKEN t = proximoToken(); t == COMA; t = proximoToken()) {
+    match(ID);
+  }
+  return;
 }
 
 int main() {
