a4dc9d2bc54c167af551e96a0174fdc4e6f9d164
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2017-10-24 18:01:29 -0300

    arreglo automata constantes

--- Tp-Sintaxis/parser/automata.c
+++ Tp-Sintaxis/parser/automata.c
@@ -1,11 +1,11 @@
 #include "headers/automata.h"
 
-const int TT[6][4] = {{5,1,5,5},
-                      {1,1,3,2},
-                      {5,5,4,5},
-                      {5,5,5,4},
-                      {5,5,5,5},
-                      {5,5,5,5}};
+const int TT[6][5] = {{5,1,5,5,5},
+                      {1,1,3,2,5},
+                      {5,5,4,5,5},
+                      {5,5,5,4,5},
+                      {5,5,5,5,5},
+                      {5,5,5,5,5}};
 
 int columna(char c) {
   if (c == '0') {
@@ -14,23 +14,24 @@ int columna(char c) {
     return 1;
   } else if (c == 'u' || c == 'U') {
     return 2;
-  } else {
+  } else if (c == 'l' || c == 'L') {
     return 3;
+  } else {
+    return 4;
   }
 }
 
 int esConstanteNumerica(char cadena[]) {
   int i = 0;
   int estado = 0;
-
-  while (cadena[i] != '\0' && estado != 5) {
-      int col = columna(cadena[i]);
-      estado = TT[estado][col];
-      i++;
+  while (cadena[i] != '\0' && estado != 5){
+    int col = columna(cadena[i]);
+    estado = TT[estado][col];
+    i++;  
   }
-  
-  return estado == 1
-      || estado == 2
-      || estado == 3
-      || estado == 4;
+
+  return estado == 1 
+      || estado == 2 
+      || estado == 3 
+      || estado == 4;  
 }
